0efa08bea6108a132c561e85c2baafa90d1a2874
Ian Klemm <hi@ianklemm.de>
2023-04-04 22:50:57 +0200

    wip

--- ubuntu-22-04.sh
+++ ubuntu-22-04.sh
@@ -15,7 +15,7 @@ wget $IMAGE_URL
 #########################################################
 # Image specific 
 
-virt-customize -a $DISK_IMAGE --install qemu-guest-agent --install resolvconf --update --run-command 'systemctl enable qemu-guest-agent' --run-command 'systemctl restart qemu-guest-agent' --run-command 'rm -f /etc/ssh/sshd_config.d/60-cloudimg-settings.conf'
+virt-customize -a $DISK_IMAGE --install qemu-guest-agent --install resolvconf --update --run-command 'systemctl enable qemu-guest-agent' --run-command 'systemctl stop qemu-guest-agent' --run-command 'systemctl start qemu-guest-agent' --run-command 'rm -f /etc/ssh/sshd_config.d/60-cloudimg-settings.conf'
 
 
 
